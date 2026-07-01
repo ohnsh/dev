8181ca11654df010ecd03631132c7e849a150406
John Sherrell <dev@ohn.sh>
2026-07-01 02:17:44 -0500

    stream-utils: installed @aws-sdk/client-s3, consolidated some ffmpeg commands.

--- _/stream-utils/mkmanifest/r2.ts
+++ _/stream-utils/mkmanifest/r2.ts
@@ -8,8 +8,8 @@ export const r2 = new S3Client({
   endpoint: `https://${process.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   region: 'auto',
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
   },
 })
 
--- _/stream-utils/mkpip.sh
+++ _/stream-utils/mkpip.sh
@@ -24,19 +24,33 @@ pip_mask_shadow() {
 [pip_v][mask]alphamerge[pip_rounded];
 color=s=480x270:c=black@0.6,pad=580:370:50:50:0x00000000,boxblur=10:5[shadow];
 [shadow][pip_rounded]overlay=50:50[pip_with_shadow];
-[main][pip_with_shadow]overlay=W-w-20:20
-" \
+[main][pip_with_shadow]overlay=W-w-20:20" \
     -c:v libx264 "$@"
 }
 
+pip_melt() {
+  # out=300 means the clip stops after 300 frames
+  # (there must be a better way to limit duration)
+  melt -profile atsc_1080p_30 \
+    "$1" out=300 \
+    -filter crop center=0 left=0 top=0 right=1920 bottom=1080 \
+    -track \
+    "$1" out=300 \
+    -filter crop center=0 left=1920 top=0 right=0 bottom=1080 \
+    -filter qtcrop radius=.2 \
+    -filter dropshadow radius=.1 \
+    -transition affine rect="1340/700:500x281" \
+    -consumer avformat:"$2" vcodec=libx264 acodec=aac
+}
+
 case "$1" in
-pip_basic | pip_mask_shadow)
+pip_basic | pip_mask_shadow | pip_melt)
   CMD=$1
   shift
   $CMD "$@"
   ;;
 *)
-  echo "Specify either pip_basic or pip_mask_shadow subcommand." >&2
+  echo "Specify a subcommand: pip_basic | pip_mask_shadow | pip_melt" >&2
   exit 1
   ;;
 esac
