55170707ba05c4ba2bac2ab3b9bb79b67ff12b9a
John Sherrell <dev@ohn.sh>
2026-07-02 18:42:03 -0500

    Added custom font (DejaVu Sans for no particular reason) to mktitle with logic to find the .ttf on Alpine or MacOS.

--- _/stream-utils/mktitle.sh
+++ _/stream-utils/mktitle.sh
@@ -1,20 +1,25 @@
+font=/usr/share/fonts/dejavu/DejaVuSans.ttf
+
+# picking dejavu sans completely randomly.
+[[ -e $font ]] || font=$HOME/Library/Fonts/DejaVuSans.ttf
+
 title() {
   # d = duration, r = framerate
   ffmpeg -f lavfi -i "color=color=#1a1a2e:s=1920x1080:d=5:r=30" \
-    -vf "drawtext=fontfile=/path/to/font.ttf:text='STREAM STARTING SOON':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2" \
+    -vf "drawtext=fontfile=$font:text='STREAM STARTING SOON':fontcolor=white:fontsize=72:x=(w-text_w)/2:y=(h-text_h)/2" \
     -c:v libx264 -pix_fmt yuv420p -g 60 title_card.mp4
 }
 
 title_gradient() {
   ffmpeg -f lavfi -i "nullsrc=s=1920x1080:d=5:r=30" \
     -vf "geq=r='X/W*255':g='0':b='Y/H*255', gblur=sigma=100,
-     drawtext=fontfile=/path/to/font.ttf:text='LIVE STREAMING':fontcolor=white:fontsize=64:x=(w-text_w)/2:y=(h-text_h)/2" \
+     drawtext=fontfile=$font:text='LIVE STREAMING':fontcolor=white:fontsize=64:x=(w-text_w)/2:y=(h-text_h)/2" \
     -c:v libx264 -pix_fmt yuv420p -g 60 title_card.mp4
 }
 
 title_retro() {
   ffmpeg -f lavfi -i "smptebars=s=1920x1080:d=5:r=30" \
-    -vf "drawtext=fontfile=/path/to/font.ttf:text='INITIALIZING FEED...':fontcolor=white:box=1:boxcolor=black@0.8:boxborderw=20:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2" \
+    -vf "drawtext=fontfile=$font:text='INITIALIZING FEED...':fontcolor=white:box=1:boxcolor=black@0.8:boxborderw=20:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2" \
     -c:v libx264 -pix_fmt yuv420p -g 60 title_card.mp4
 }
 
@@ -28,8 +33,8 @@ title_silent_audio() {
 title_bgimage() {
   # -r and -g are for RTMP stability (match subsequent inputs)
   ffmpeg -loop 1 -i background.png -t 5 \
-    -vf "drawtext=fontfile=/path/to/font.ttf:text='STREAM STARTING SOON':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2,
-     drawtext=fontfile=/path/to/font.ttf:text='%{localtime\:%X}':fontcolor=yellow:fontsize=32:x=(w-text_w)/2:y=(h-text_h)/2+60" \
+    -vf "drawtext=fontfile=$font:text='STREAM STARTING SOON':fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2,
+     drawtext=fontfile=$font:text='%{localtime\:%X}':fontcolor=yellow:fontsize=32:x=(w-text_w)/2:y=(h-text_h)/2+60" \
     -c:v libx264 -pix_fmt yuv420p -r 30 -g 60 title_card.mp4
 }
 
