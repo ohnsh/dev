d78059823deffee71a15aa7f01a0821566f56a63
John Sherrell <dev@ohn.sh>
2026-07-11 05:34:26 -0500

    bug: Documentation. Only pass `--add-host` when we actually have an mdns name to pre-resolve.

--- _/bug/bug-alpine.sh
+++ _/bug/bug-alpine.sh
@@ -2,6 +2,9 @@
 
 BUGDIR=${BUGDIR:-/recordings}
 
+# When in a container, no mdns resolver is available but the host
+# mapping is added via the command line. In that case, just leave the
+# hostname in PULSE_SERVER and let the system resolve it.
 maybe_resolve() {
   local host=$1
   if [[ $host == *.local ]] && which avahi-resolve >/dev/null 2>&1; then
@@ -10,17 +13,18 @@ maybe_resolve() {
     echo "$host"
   fi
 }
-# pulseaudio source names on macOS are incredibly unhelpful. For Samson G-Track Pro:
-# pactl get-default-source
-# Front_Left__Front_Right.2
+
 bug_ffmpeg_pulse() {
-  local host=${1:-mak.local}
-  local seg_length=3600
-  host=$(maybe_resolve "$host")
+  local seg_length=3600 # one hour
 
-  export PULSE_SERVER="${PULSE_SERVER:-tcp:$host:4713}"
-  echo "Starting audio recording from server: $PULSE_SERVER"
-
+  # IMPORTANT: default source must be configed in pulseaudio.
+  #
+  # Background: pulseaudio source names on macOS are incredibly unhelpful.
+  # For Samson G-Track Pro, `pactl list sources short` gives:
+  #
+  # Front_Left__Front_Right.2
+  #
+  # Instead, we'll use the default source, which must be configured on the server.
   exec ffmpeg -hide_banner -y \
     -f pulse -i "default" \
     "${codec[@]}" \
@@ -28,21 +32,30 @@ bug_ffmpeg_pulse() {
     -reset_timestamps 1 -segment_atclocktime 1 \
     -strftime 1 -use_wallclock_as_timestamps 1 \
     "$BUGDIR/bug_%Y%m%d_%H%M%S.$ext"
-    #-use_localtime 1 # not working, perhaps only valid for hls segments
 }
 
 bug_aac() {
   local codec=(-c:a aac -b:a 128k)
   local ext=aac
-  bug_ffmpeg_pulse "$@"
+  bug_ffmpeg_pulse
 }
 
 bug_opus() {
   local codec=(-c:a libopus -b:a 64k)
   local ext=opus
-  bug_ffmpeg_pulse "$@"
+  bug_ffmpeg_pulse
 }
 
 mkdir -p "$BUGDIR"
 
-bug_opus "$@"
+host=${1:-mak.local}
+# override PULSE_SERVER if an explicit host is passed.
+if [[ -z $PULSE_SERVER ]] || [[ -n $1 ]]; then
+  PULSE_SERVER="tcp:$(maybe_resolve "$host"):4713"
+fi
+
+export PULSE_SERVER
+echo "Recording audio from server: $PULSE_SERVER" >&2
+
+# Should make format (aac or opus) configurable via environment. For now, just use opus.
+bug_opus
--- _/bug/run-bug.sh
+++ _/bug/run-bug.sh
@@ -3,23 +3,35 @@
 BUGDIR=/recordings
 host_bugdir=$HOME/Export/bug
 
-mdns_resolve() {
-  avahi-resolve -4 -n "$1" | cut -f 2
+usage() {
+  cat <<EOF
+Usage: $(basename "$0") [-has] [HOSTNAME]
+
+Options:
+  -h    Display this help and exit
+  -a    Attach to existing container
+  -s    Open shell in existing container
+
+Arguments:
+  HOSTNAME    The hostname or IP address of the pulseaudio server. Default: 'mak.local'
+
+Description:
+  Continuously record the default source from a pulseaudio server
+  to aac or opus files. Default segment length is one hour.
+
+EOF
 }
 
-_main() {
-  docker run -dit \
-    --name "$name" \
-    --user "$(id -u):$(id -g)" \
-    --restart unless-stopped \
-    --add-host "$host:$(mdns_resolve "$host")" \
-    -e "BUGDIR=$BUGDIR" -e "TZ=$TZ" \
-    -v "$host_bugdir:$BUGDIR" \
-    "$image"
+mdns_resolve() {
+  local host=$1
+  avahi-resolve -4 -n "$host" | cut -f 2
 }
 
 mode=start
-if [[ $1 == "-a" ]]; then
+if [[ $1 == "-h" ]]; then
+  usage
+  exit 0
+elif [[ $1 == "-a" ]]; then
   mode=attach
   shift
 elif [[ $1 == "-s" ]]; then
@@ -33,9 +45,30 @@ name=bug
 mkdir -p "$host_bugdir"
 
 if [[ $mode == start ]]; then
-  _main
+  # No mdns resolver available in the container, so we'll pass in the mapping via the
+  # command line.
+  declare -a host_mapping
+  if [[ $host == *.local ]]; then
+    host_mapping=(--add-host "$host:$(mdns_resolve "$host")")
+  fi
+
+  # run detached but allocate a pty so that ffmpeg's terminal logging
+  # looks right if/when we do attach.
+  # -i is probably silly but it seemed to help with an issue where
+  # the detach key sequence wasn't working.
+  docker run -dit \
+    --name "$name" \
+    --user "$(id -u):$(id -g)" \
+    --restart unless-stopped \
+    "${host_mapping[@]}" \
+    -e "BUGDIR" -e "TZ" \
+    -v "$host_bugdir:$BUGDIR" \
+    "$image"
+
 elif [[ $mode == attach ]]; then
+  # Attach to existing bug.
   docker attach "$name" || docker start -ai "$name"
 else
+  # Get a bash shell inside existing bug.
   docker start "$name" && docker exec -it "$name" /bin/bash
 fi
