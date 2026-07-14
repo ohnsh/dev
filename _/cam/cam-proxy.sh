91c51528ef54c876ae7aed7a18298d14ad422985
John Sherrell <dev@ohn.sh>
2026-07-13 21:02:07 -0500

    cam: Added timezone awareness to mediamtx container (tzdata package and TZ environment variable). Added logic to only pre-resolve .local hostnames and only include --add-hosts on the docker command line when necessary. Added stream key redaction (from ffmpeg logs) to stream_folder.sh.

---
+++ _/cam/cam-proxy.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+mdns_resolve() {
+  local host=$1
+  if command -v avahi-resolve &>/dev/null; then
+    avahi-resolve -4 -n "$host" | awk '{ print $2 }'
+  else
+    getent hosts "$host" | awk '{ print $1 }'
+  fi
+}
+
+CAM_USER=thingino
+CAM_PASS=thingino
+CAM_HOST=${CAM_HOST:-ing-wuuk.local}
+
+host_mapping=()
+if [[ $CAM_HOST == *.local ]]; then
+  CAM_IP=$(mdns_resolve "$CAM_HOST") || {
+    echo "Error resolving mdns name $CAM_HOST. (Is avahi-resolve installed?)" >&2
+    exit 1
+  }
+  host_mapping=(--add-host "$CAM_HOST:$CAM_IP")
+fi
+
+CAM_SOURCE=rtsp://$CAM_USER:$CAM_PASS@$CAM_HOST:554/ch0
+
+if [[ -d $HOME/Export ]]; then
+  RECORDINGS_HOST=$HOME/Export/cam
+else
+  RECORDINGS_HOST=$PWD/recordings
+fi
+# prevent docker from creating directory with root ownership
+mkdir -p "$RECORDINGS_HOST"
+
+run() {
+  docker run -dit \
+    --name cam-proxy \
+    --restart unless-stopped \
+    --user "$(id -u):$(id -g)" \
+    -e "TZ" \
+    -e MTX_RTSPTRANSPORTS=tcp \
+    -e MTX_PATHS_WUUK_SOURCE="$CAM_SOURCE" \
+    -p 8554:8554 \
+    -p 1935:1935 \
+    -p 8888:8888 \
+    -p 8889:8889 \
+    -p 8890:8890/udp \
+    -p 8189:8189/udp \
+    -v ./mediamtx.yml:/mediamtx.yml:ro \
+    -v "$RECORDINGS_HOST:/recordings" \
+    "${host_mapping[@]}" \
+    cam-proxy
+
+  # bluenviron/mediamtx:latest-ffmpeg
+  # -e MTX_WEBRTCADDITIONALHOSTS=192.168.x.x \
+}
+
+build() {
+  docker build -t cam-proxy .
+}
+
+cmd=run
+case "$1" in
+run | build)
+  cmd=$1
+  shift
+  ;;
+esac
+
+$cmd
--- _/cam/stream_folder.sh
+++ _/cam/stream_folder.sh
@@ -11,19 +11,54 @@ load_dotenv() {
 
   while [[ -n $dir ]]; do
     for dotenv in "$dir"/.env{,.*}; do
+      # always expands to at least .env, even if it doesn't exist
       [[ -f "$dotenv" ]] || continue
       stack+=("$dotenv")
     done
+    # Don't walk past project root (marked by package.json OR .git)
     [[ -f "$dir/package.json" || -d "$dir/.git" ]] && break
+    # Move to parent directory for next iteration
     dir=${dir%/*}
   done
 
+  # Source dotenv files with allexport set so that environment is modified
+  # without the files needing an explicit `export` on every line.
   set -o allexport
+
+  # Start at the top of the stack by looping backwards. This ensures that
+  # nearer/more specific dotenv files override those closer to the project root.
   for ((i = 1; i <= ${#stack[@]}; i++)); do
     dotenv=${stack[-i]}
     . "$dotenv"
   done
   set +o allexport
+}
+
+redact_tty() {
+  # This is mostly academic; I don't care much if the stream key is logged.
+  # But I am screen-recording the output at times, so there is some logic to redacting it.
+  local pfx="youtube\.com\/live2\/"
+  # macOS script options are not compatible with alpine/util-linux script.
+  # The awk script is Gemini's suggestion to handle ffmpeg's tty-connected log output
+  # (among other issues, filled with \r instead of \n) in a graceful and unbuffered way.
+  # Previously, I used `sed -u "s/${pfx}.*/${pfx}[redacted]/g"`.
+  script -q /dev/null -- "$@" |
+    awk -f <(
+      cat <<EOF
+    BEGIN {
+        RS = "[\r\n]"
+    }
+    {
+        gsub(/${pfx}.*/, "[REDACTED]")
+
+        # Print the line and manually append the matched separator (CR or LF)
+        printf "%s%s", \$0, RT
+        fflush()
+    }
+EOF
+    )
+
+  return "${PIPESTATUS[0]}"
 }
 
 stream_folder() {
@@ -39,7 +74,8 @@ stream_folder() {
   mkdir -p "$out_dir"
 
   for movie in "$in_dir"/*.mp4; do
-    ffmpeg \
+    redact_tty ffmpeg \
+      -hide_banner \
       -readrate 1 -readrate_catchup 2 \
       -i "$movie" \
       -c copy \
