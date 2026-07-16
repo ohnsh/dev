4065f61146a9bfcd6226d6ee3ec88601e5b49170
John Sherrell <dev@ohn.sh>
2026-07-15 22:15:29 -0500

    cam: Document correct usage of monitor.sh: The status FIFO must be open for read/write (<>$STATUS_FIFO).

--- _/cam/monitor.sh
+++ _/cam/monitor.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+usage() { cat; } <<EOF
+Usage:
+  $(basename "$0") <> "\$STATUS_FIFO"
+
+Description:
+  Opening the status FIFO for read/write is crucial so that EOF isn't sent on
+  every status update. This is because the FIFO always has at least one writer:
+  $(basename "$0") itself.
+
+EOF
+
+if [[ $# -ne 0 ]]; then
+  usage
+  exit 1
+fi
+
 while read -r script status; do
   echo "$script: $status" >&2
   curl -d "$script: $status" \
